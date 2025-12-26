26791b15d693df3329c56e026d35c90306e5ac33
Tristan Poland (Trident_For_U) <34868944+tristanpoland@users.noreply.github.com>
2025-12-26 15:29:27 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/the-rust-bible.github.io' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '' : '',
 };
 
 export default nextConfig;
