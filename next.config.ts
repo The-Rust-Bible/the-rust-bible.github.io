a7957114969d30cb4af4e0e55d68987ae38d2b02
Conner <Conneradamsmaine@gmail.com>
2025-12-26 16:58:38 -0500

    Build optimizations

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '' : '',
   reactStrictMode: true,
+  compress: true,
   // Force all pages to be static
   trailingSlash: true,
 };
